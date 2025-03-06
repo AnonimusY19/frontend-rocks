5b47239c654ebb7f198aad79e31d70ad1d111967
AnonimusY19 <129310961+AnonimusY19@users.noreply.github.com>
2025-03-06 15:10:02 +0000

    Modify root

--- src/app/router/index.tsx
+++ src/app/router/index.tsx
@@ -6,7 +6,7 @@ import { DetailRoute } from "../../routes/detail/index.tsx";
 export const AppRouter = () => {
     return <BrowserRouter>
         <Routes>
-            <Route path="/" element={<RootRoute />}></Route>
+            <Route path="/frontend-rocks" element={<RootRoute />}></Route>
             <Route path="/frontend-rocks/dettaglio/:id" element={<DetailRoute />}></Route>
         </Routes>    
     </BrowserRouter>
